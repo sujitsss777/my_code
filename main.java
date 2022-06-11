8ab266c60901a494865999fcaa0f15a98c332929
Sujit Suryawanshi <71975563+sujitsss777@users.noreply.github.com>
2022-06-11 10:16:51 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,19 @@
+package project;
+
+import java.util.List;
+
+public class main 
+{
+public static void main(String[] args) 
+{
+ 
+     int b=10;
+      a=a+b;
+      b=a-b;
+      a=a-b;
+      System.out.println(a);
+      System.out.println(b);
+  
+    
+}
+}
